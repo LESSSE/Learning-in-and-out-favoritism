808ea341b8e7f1068e24254fd0b43d0a1c97f6bf
diogo-abreu123 <diogo.abreu2012@gmail.com>
2017-11-10 23:51:19 +0000

    Partial implementation of Play() and Update_Fitness()

--- in-and-out-favoritism/in-and-out-favoritism/Agent.cpp
+++ in-and-out-favoritism/in-and-out-favoritism/Agent.cpp
@@ -10,3 +10,20 @@ Agent::Agent()
 Agent::~Agent()
 {
 }
+
+void Agent::Play(bool same_group) 
+{
+	if (same_group) 
+	{
+		//cooperate
+	}
+	else 
+	{
+		//generate random number that will determine if agent cooperates or defects.
+	}
+}
+
+void Agent::Update_Fitness(float new_value) 
+{
+	fitness = new_value;
+}
