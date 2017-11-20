26c6b519ae102f869ddda18d59f410020b753246
LESSSE <luis.carlos.as@gmail.com>
2017-11-20 17:27:45 +0000

    WORKING VERSION

---
+++ in-and-out-favoritism/in-and-out-favoritism/Group.h
@@ -0,0 +1,10 @@
+#ifndef GROUP_H
+#define GROUP_H
+
+typedef int Group; 
+
+/* in this implementation:
+ *   a group is an integer >= 0
+*/
+
+#endif
---
+++ in-and-out-favoritism/in-and-out-favoritism/Strategy.h
@@ -0,0 +1,13 @@
+#ifndef STRATEGY_H
+#define STRATEGY_H
+
+#include <utility>
+
+typedef std::pair <float,float> Strategy;
+
+/* in this implementation the Strategy is just a pair
+ * 	the first denotes the probability to cooperate with an agent of the same set
+ * 	the second denotes the probability to cooperate with an agent of a different set*/
+
+#endif
+
