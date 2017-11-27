56cef3f7521e3bb8aba70e7fae77ae53f3c7f99a
LESSSE <luis.carlos.as@gmail.com>
2017-11-27 11:10:10 +0000

    Added func and dev

--- in-and-out-favoritism/in-and-out-favoritism/main.cpp
+++ in-and-out-favoritism/in-and-out-favoritism/main.cpp
@@ -1,23 +1,64 @@
-#include "Coop_Game.h"
+#include "CoopGame.h"
 #include "Agent.h"
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <cmath>
+#include <algorithm>
 
+//NUMBERS
 #define N_AGENTS 1000
-#define N_STEPS 1000
-#define N_TRAIN 1000
+#define N_STEPS 100000
+#define N_TRAIN 10000
+
+//PROBABILITIES
 #define A_IN 1
 #define A_OUT 0.6
 
+#define BETA 1
+#define DEV 0.01 
+
+
+float func(float x, float y){
+	float diff = x - y;
+	return 1 / (1 + std::exp(- BETA * diff));
+}
+
+float dev(){
+	float r;
+	r = ((double) rand() / (RAND_MAX));
+	r -= 0.5;
+	r *= DEV;
+	return r;
+}
+
+void getFitness(Interaction* interaction, Agent** agents, int agent){
+	int a;
+	for(int i = 0; i < N_TRAIN; i++){
+		a = rand() % N_AGENTS;
+		if(a == agent)
+			continue;
+		else{
+			if(agents[a]->set == agents[agent]->set){
+				interaction->play(agents[agent],agents[a]);
+				continue;
+			}	
+			if( (((double) rand()) / (RAND_MAX)) < A_OUT){
+				interaction->play(agents[agent],agents[a]);
+				continue;
+			}
+		}
+	}	
+}
+
 int main(){
-	int N_GROUPS = 5; 
+	int N_GROUPS = 20; 
 	int* groups;
-	int a1, a2, a3, a4;
+	int a1, a2;
 	Agent** agents;
-	Coop_Game* pris_dilemma = new Coop_Game(1.5, -0.5);
-	Coop_Game* stag_hunt = new Coop_Game(0.5, -0.5);
-	Coop_Game* snowdrift = new Coop_Game(1.5, 0.5);
+	CoopGame* pris_dilemma = new CoopGame(1.5, -0.5);
+	//CoopGame* stag_hunt = new CoopGame(0.5, -0.5);
+	//CoopGame* snowdrift = new CoopGame(1.5, 0.5);
 
 	Interaction* interaction = pris_dilemma;
 
@@ -35,55 +76,30 @@ int main(){
 		int group = rand() % N_GROUPS;
 		agents[i] = new Agent(group);
 		groups[group]++;
-		printf("GROUP %d\n", agents[i]->set);
+		/*
+		 printf("GROUP %d\n", agents[i]->set);
 		printf("STRATEGY (%f,%f)\n", agents[i]->strategy.first, agents[i]->strategy.second);
 		printf("___________________________\n");
+		*/
 	}
 
 	for(int i = 0; i < N_GROUPS; i++){
 		printf("GROUP %d: %d\n", i, groups[i]);
 	}
 
-	printf("______INITIALIZAITON_____");
+	printf("______TRAIN_____");
 
 	for(int i = 0; i < N_STEPS; i++){
-		a1 = rand() % N_AGENTS; 
-		for(int i = 0; i < N_TRAIN; i++){
-			a2 = rand() % N_AGENTS;
-		       	if(a1 == a2)
-				continue;
-			else{
-				if(agents[a1]->set == agents[a2]->set){
-					interaction->play(agents[a1],agents[a2]);
-					continue;
-				}
-				if( (((double) rand())/ RAND_MAX ) < A_OUT){
-					interaction->play(agents[a1],agents[a2]);
-					continue;
-				}
-			}
-		}
-		
-		a3 = rand() % N_AGENTS; 
-		for(int i = 0; i < N_TRAIN; i++){
-			a4 = rand() % N_AGENTS;
-		       	if(a3 == a4)
-				continue;
-			else{
-				if(agents[a3]->set == agents[a4]->set){
-					interaction->play(agents[a3],agents[a4]);
-					continue;
-				}
-				if( (((double) rand())/ RAND_MAX ) < A_OUT){
-					interaction->play(agents[a3],agents[a4]);
-					continue;
-				}
-			}
-		}
-
-		if( agents[a3]->fitness > agents[a1]->fitness){
-			agents[a1]->strategy.first = agents[a3]->strategy.first;
-			agents[a1]->strategy.second = agents[a3]->strategy.second;
+		int r;
+		a1 = rand() % N_AGENTS;
+		getFitness(interaction, agents,a1);
+		a2 = rand() % N_AGENTS; 
+		getFitness(interaction, agents,a2);
+		r = ((double) rand() / (RAND_MAX));	
+		if( r <= func(agents[a2]->fitness,agents[a1]->fitness)){
+			agents[a1]->strategy.first = agents[a2]->strategy.first + dev();
+			agents[a1]->strategy.second = agents[a2]->strategy.second + dev(); 
+			printf("STRATEGY CHANGE (%f,%f) (%f,%f)\n",agents[a1]->strategy.first, agents[a1]->strategy.second + dev(), agents[a2]->strategy.first, agents[a2]->strategy.second);	
 		}
 	}
 
@@ -94,6 +110,5 @@ int main(){
 		printf("FITNESS %f\n", agents[i]->fitness);
 		printf("___________________________\n");
 	}
-		
 	return 0;
 }
